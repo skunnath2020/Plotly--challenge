73049f2559b59d62aa9060bb5993ce6b7e7d5d29
skunnath2020 <sskunnath@gmail.com>
2021-02-28 10:06:32 -0500

    bonus wip

--- static/js/app.js
+++ static/js/app.js
@@ -17,8 +17,6 @@ function init() {
 function createDemographics(id){
     d3.json("data/samples.json").then((data) => {
         var metadata= data.metadata;
-        console.log("Here")
-        console.log(metadata);
         var filteredId = metadata.filter(d => d.id.toString() === id);
         showDemographics(filteredId);
     
@@ -26,7 +24,6 @@ function createDemographics(id){
 }  
 function showDemographics(metadata){
     demographics = d3.select("#sample-metadata")
-    console.log(metadata[0] )
     //clear the panel 
     demographics.html('');
     //get the key/value pair
@@ -39,7 +36,6 @@ function showDemographics(metadata){
 function createChart(id){
     d3.json("data/samples.json").then((data) => {
         var filtered = data.samples.filter(d => d.id.toString() === id)[0];
-        console.log("OTU " , filtered)
         // Top ten OTU_ids and sample values
         var toptenOTU = filtered
                         .otu_ids.slice(0, 10)
@@ -64,13 +60,21 @@ function createChart(id){
             orientation: "h"
          };
          var layout = {
-             title: "Top ten OTUs",
+             title: "<b>Top ten OTUs</b>",
+             height: 500,
+		  	 width: 600,
              "titlefont": {
-                "size": 18
+                "size": 20
               },
              yaxis: {
                  tickmode: "linear",
-             }
+             },
+             margin: {
+                l: 100,
+                r: 100,
+                t: 120,
+                b: 30
+            }
          };
          Plotly.newPlot("bar", [trace], layout)
     
@@ -93,6 +97,39 @@ function createChart(id){
             width: 1000
         }
         Plotly.newPlot(bubble, [trace1], bubble_layout)
+        // Build guage chart
+        var metadata = data.metadata.filter(d => d.id.toString() === id)[0];
+        var wfreq = metadata.wfreq;
+        if (wfreq == null) {
+            wfreq = 0;
+        }
+        var data = [
+            {
+                domain: { x: [0, 1], y: [0, 1]  },            
+                value: parseFloat(wfreq),
+                title: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week<br><br>',
+		  	    height: 600,
+		  	    width: 600,
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: { axis: { range: [0, 9] },
+                bar: { color: 'rgba(8,29,88,0)' },
+                   steps: [
+                    { range: [0, 1], color: 'rgb(255,255,217)' },
+                    { range: [1, 2], color: 'rgb(237,248,217)' },
+                    { range: [2, 3], color: 'rgb(199,233,180)' },
+                    { range: [3, 4], color: 'rgb(127,205,187)' },
+                    { range: [4, 5], color: 'rgb(65,182,196)' },
+                    { range: [5, 6], color: 'rgb(29,145,192)' },
+                    { range: [6, 7], color: 'rgb(34,94,168)' },
+                    { range: [7, 8], color: 'rgb(37,52,148)' },
+                    { range: [8, 9], color: 'rgb(8,29,88)' }
+                  ]}
+            }
+        ];
+        
+        var layout = { width: 600, height: 500, margin: { t: 10, b: 0 } };
+        Plotly.newPlot('gauge', data, layout);
 });
 }
 
